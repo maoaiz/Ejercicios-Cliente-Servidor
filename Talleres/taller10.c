d7d6c6e855fccfc861b2a0ad70a2fd04288bdf70
Mauricio Aizaga <mauricioaizaga@gmail.com>
2015-04-16 19:36:39 -0500

    4 hilos. cada hilo escribe cada i segundos el número i en la próxima posición

---
+++ Talleres/taller10.c
@@ -0,0 +1,18 @@
+/**
+ * Taller 10. Crear un buffer con capacidad  para contener TAM_BUFFER 
+ * bytes. Crear 4 hilos {t1, t2, t3, t4}, tales que:
+ * 
+ *  * cada ti escribe cada i segundos el número i en la próxima posición
+ *    libre de p del buffer, tras lo cual escribe nen stdout:
+ *    'escribí un i en la posición p'
+ *  * ni bien un ti descubre que el buffer está lleno, termina.
+ * 
+ * 
+ * 
+ * Created by: Mauricio Aizaga
+ * Marzo 26 de 2014
+ * github.com/maoaiz
+ * Universidad Tecnologica de Pereira
+ * 
+ * /
+
